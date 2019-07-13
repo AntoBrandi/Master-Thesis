a5c02532dd298c357bedd34fa649776852ae1691
anton <antonio.brandi@outlook.it>
2019-07-13 17:37:45 +0200

    Starting point for v2.0

---
+++ Android-Application/MyIoTdevice/app/src/main/java/com/example/myiotdevice/FirstSensorActivity.java
@@ -0,0 +1,15 @@
+package com.example.myiotdevice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstSensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firts_sensor_activity);
+    }
+}
+
